68fb568ef8f0453eaf93149d4989c719ff78fc60
chu621006 <chu621006@gmail.com>
2025-08-05 13:44:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pdfplumber>=0.10.0
+import pdfplumber
 import collections
 import re 
 
@@ -692,4 +692,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
